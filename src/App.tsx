44d4d6c78a567c42bb8a7cc57fe9e8d5afe669be
Omar Khaled <omarkhaled2440@gmail.com>
2023-11-14 22:51:02 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+
+import {Routes, Route} from "react-router-dom"
+import { Container } from "react-bootstrap"
+import './App.css'
+import Home from "./pages/Home"
+import Store from "./pages/Store"
+import About from "./pages/About"
+import NavBar from "./components/NavBar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+
+
+function App() {
+
+
+  return (
+    <>
+    <ShoppingCartProvider>
+    <NavBar></NavBar>
+    <Container className="mb-4">
+  <Routes>
+    <Route path="/" element={<Home/>}></Route>
+    <Route path="/store" element={<Store/>}></Route>
+    <Route path="/about" element={<About/>}></Route>
+  </Routes>
+    </Container>
+    </ShoppingCartProvider>
+    </>
+  )
+}
+
+export default App
